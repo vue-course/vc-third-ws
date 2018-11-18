ba48afc1aa5c4578bb1f4084f87791290e70e9c2
lironh <lironenk@gmail.com>
2018-11-18 13:33:47 +0200

    :sparkle: added stages module

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import task from "./modules/task/task.module";
+import stages from "./modules/stages/stages.module";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
         task,
+        stages
     }
 });
